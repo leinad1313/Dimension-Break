b2bb8e7cad66bc98451cc9abb7c4479eef009579
leinad1313 <48999650+leinad1313@users.noreply.github.com>
2020-02-19 12:56:33 +0100

    Save system for settings (base for future Savegame system)

--- Scripts/GUI/MainMenuHandler.gd
+++ Scripts/GUI/MainMenuHandler.gd
@@ -2,11 +2,15 @@ extends Control
 
 
 onready var Root = get_node("/root")
+onready var Config = get_node("/root/ConfigHandler")
+onready var saveData = Config.load_settings()
 onready var MainMenu = get_node("MainMenu")
 onready var OptionsMenu = get_node("OptionsMenu")
-onready var ResolutionButton = get_node("OptionsMenu/HBoxContainer/VBox1/HBoxResolution/OptionButton")
 onready var VolumeSlider = get_node("OptionsMenu/HBoxContainer/VBox1/HBoxMasterVolume/HSlider")
-onready var MusicVolumeSlider = get_node("OptionsMenu/HBoxContainer/VBox2/HBoxMusicVolume/HSliderMusic")
+onready var MusicVolumeSlider = get_node("OptionsMenu/HBoxContainer/VBox1/HBoxMusicVolume/HSliderMusic")
+
+
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,22 +18,8 @@ func _ready():
 	var mainInnerContainer = get_node("MainMenu/Container/InnerContainer")
 	var tweenMain = get_node("MainMenu/Container/InnerContainer/Tween")
 	var mainInnerStartRect = mainInnerContainer.rect_position
-	
-	var RawResolution = Root.get_visible_rect()
-	var RawResolutionSize = RawResolution.size
-	var FinalResolution = str(RawResolutionSize.floor()).replace("(", "").replace(")", "").replace(", ", "x")
-	if FinalResolution == "1280x720":
-		ResolutionButton.select(0)
-	elif FinalResolution == "1920x1080":
-		ResolutionButton.select(1)
-	elif FinalResolution == "2540x1440":
-		ResolutionButton.select(2)
-	else:
-		ResolutionButton.select(3)
-	
-	ResolutionButton.text = FinalResolution
-	MusicVolumeSlider.value = db2linear((AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music"))))*100
-	VolumeSlider.value = db2linear((AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))))*100
+	MusicVolumeSlider.value = saveData["audio"]["music"]
+	VolumeSlider.value = saveData["audio"]["master"]
 	tweenMain.interpolate_property(mainInnerContainer, "rect_position", mainInnerStartRect-Vector2(3000,320), mainInnerStartRect+Vector2(0,0), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tweenMain.start()
 
@@ -52,32 +42,20 @@ func _on_Options_pressed():
 
 func _on_BackOptions_pressed():
 	OptionsMenu.hide()
+	Config.save_settings(saveData)
 	MainMenu.show()
 
 
 
 func _on_HSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
+	saveData["audio"]["master"] = value
 
-
-
-func _on_OptionButton_item_selected(id):
-	if id == 1:
-		Root.size = Vector2(1920, 1080)
-		OS.window_size = Vector2(1920, 1080)
-	elif id == 0:
-		Root.size = Vector2(1280, 720)
-		OS.window_size = Vector2(1280, 720)
-	elif id == 2:
-		Root.size = Vector2(2540, 1440)
-		OS.window_size = Vector2(2540, 1440)
-	else:
-		Root.size = Vector2(3840, 2160)
-		OS.window_size = Vector2(3840, 2160)
 
 
 func _on_HSliderMusic_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), value)
+	saveData["audio"]["music"] = value
 	if value == 50:
 		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), -100)
-
+		saveData["audio"]["music"] = -100
---
+++ Scripts/SaveSystem/ConfigHandler.gd
@@ -0,0 +1,51 @@
+extends Node
+
+const SAVE_PATH = "res://config.cfg" 
+#"user://config.cfg"
+
+
+var _config_file = ConfigFile.new()
+var _settings = {
+	"audio": {
+		"master": db2linear((AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))))*100,
+		"music": db2linear((AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music"))))*100
+	}
+}
+
+func _ready():
+	var error = _config_file.load(SAVE_PATH)
+	if error != OK:
+		save_settings(_settings)
+	else:
+		print("File already exists")
+	print(load_settings())
+#	var root = get_tree().get_root()
+#	var current_scene = root.get_child(root.get_child_count() - 1)
+#	var Text =  current_scene.get_node("MainMenu/Text")
+#	Text.text = str(_settings["audio"]["master"])
+	
+
+
+func save_settings(saveData):
+	if saveData == null:
+		saveData = _settings
+	for section in saveData.keys():
+		for key in saveData[section]:
+			_config_file.set_value(section, key, saveData[section][key])
+	_config_file.save(SAVE_PATH)
+
+func load_settings():
+	var error = _config_file.load(SAVE_PATH)
+	if error != OK:
+		print("Failed loading settings file.")
+		return []
+
+	for section in _settings.keys():
+		for key in _settings[section]:
+			_settings[section][key] = _config_file.get_value(section, key, null)
+	return(_settings)
+
+
+
+
+
